7c78a06ffd2b4f7d04d3ebbd4aed7d29c7e70235
rudkevych <oksana.rudkevych@gmail.com>
2022-11-10 23:39:04 +0100

    feat: basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-function App() {
-  return (
-    <div>
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import QuoteList from './components/quotes/QuoteList';
 
-    </div>
+function App() {
+  const quotes = [{ id: 1, text: 'text' }];
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/quotes'>
+          <QuoteList quotes={quotes} />
+        </Route>
+        <Route path='/quotes/:id'>
+          
+        </Route>
+        <Route path='/new-quote'>
+          
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
